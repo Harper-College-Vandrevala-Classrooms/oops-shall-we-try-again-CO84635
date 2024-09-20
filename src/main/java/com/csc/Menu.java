1407e6fe056a3c8ade48e822e0fc0f3ac51a0a80
Owen Ceresa <co84635@mail.harpercollege.edu>
2024-09-20 18:47:49 -0500

    Update Menu.java

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -3,55 +3,125 @@ package com.csc;
 import java.util.Scanner;
 
 public class Menu {
+    public static boolean isValidInput(Integer lower, Integer upper, Integer value) {
+        if (lower != null && value < lower) return false;
+        if (upper != null && value > upper) return false;
+        return true;
+    }
 
-    public static int menuValidIntegerChecker(Integer lower, Integer upper, String prompt, String errorMessage ) {
+    public static Integer menuValidIntegerChecker(Integer lower, Integer upper, String prompt, String errorMessage) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.print(prompt);
+            System.out.print(prompt + " Enter \"exit\" to leave the menu: ");
+            String input = scanner.next();
 
-            int input = scanner.nextInt();
-
-            boolean isValid = true;
-
-            if (lower != null && input < lower) {
-                isValid = false;
-            }
-            if (upper != null && input > upper) {
-                isValid = false;
+            if (input.equals("exit")) {
+                return null;
             }
 
-            if (isValid) {
-                return input;
+            if (scanner.hasNextInt()) {
+                int value = scanner.nextInt();
+                if (isValidInput(lower, upper, value)) {
+                    return value;
+                } else {
+                    System.out.println(errorMessage);
+                }
             } else {
                 System.out.println(errorMessage);
+                scanner.next();
             }
         }
     }
 
+
+    public static Integer menuValidIntegerCheckerWithDefault(
+            Integer lower, Integer upper, String prompt, String errorMessage, int defaultValue) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print(prompt + " Enter \"default\" to use the default value of " + defaultValue + ". Or" +
+                    "Enter \"exit\" to leave the menu: ");
+            String input = scanner.next();
+
+            if (input.equals("exit")) {
+                return null;
+            }
+
+            if (input.equals("default")) {
+                if (isValidInput(lower, upper, defaultValue)) {
+                    return defaultValue;
+                } else {
+                    System.out.println(errorMessage);
+                }
+            } else if (scanner.hasNextInt()) {
+                int integerInput = scanner.nextInt();
+                if (isValidInput(lower, upper, integerInput)) {
+                    return integerInput;
+                } else {
+                    System.out.println(errorMessage);
+                }
+            } else {
+                System.out.println(errorMessage);
+                scanner.next();
+            }
+        }
+    }
+
+    public static void printResult(Integer result) {
+        if (result != null) {
+            System.out.println("Result with no bounds: " + result);
+        } else {
+            System.out.println("User exited the prompt.");
+        }
+    }
+
+
     public static void main(String[] args) {
-        String prompt = "Please enter a value: ";
+        String prompt = "Please enter a value ";
         String errorMessage = "Your value is invalid.";
 
-        int result = Menu.menuValidIntegerChecker(null, null, prompt, errorMessage);
-        System.out.println("Result with no bounds: " + result);
+        Integer result = Menu.menuValidIntegerChecker(null, null, prompt, errorMessage);
+        printResult(result);
 
         result = Menu.menuValidIntegerChecker(null, 100, prompt, errorMessage);
-        System.out.println("Result with bounds: " + result);
+        printResult(result);
 
         result = Menu.menuValidIntegerChecker(0, null, prompt, errorMessage);
-        System.out.println("Result with bounds: " + result);
+        printResult(result);
 
         result = Menu.menuValidIntegerChecker(0, 100, prompt, errorMessage);
-        System.out.println("Result with bounds: " + result);
+        printResult(result);
 
-        result = Menu.menuValidIntegerChecker(null, 100, prompt, errorMessage);
-        System.out.println("Result with bounds: " + result);
+        result = Menu.menuValidIntegerChecker(-100, 100, prompt, errorMessage);
+        printResult(result);
 
         result = Menu.menuValidIntegerChecker(-100, null, prompt, errorMessage);
-        System.out.println("Result with bounds: " + result);
+        printResult(result);
 
-        result = Menu.menuValidIntegerChecker(-100, -50, prompt, errorMessage);
-        System.out.println("Result with bounds: " + result);
+        result = Menu.menuValidIntegerChecker(-100, 0, prompt, errorMessage);
+        printResult(result);
+
+        result = Menu.menuValidIntegerCheckerWithDefault(null, null, prompt, errorMessage, 100);
+        printResult(result);
+
+
+        result = Menu.menuValidIntegerCheckerWithDefault(null, 100, prompt, errorMessage, 100);
+        printResult(result);
+
+        result = Menu.menuValidIntegerCheckerWithDefault(0, null, prompt, errorMessage, 100);
+        printResult(result);
+
+        result = Menu.menuValidIntegerCheckerWithDefault(0, 100, prompt, errorMessage, 100);
+        printResult(result);
+
+        result = Menu.menuValidIntegerCheckerWithDefault(-100, 100, prompt, errorMessage, 100);
+        printResult(result);
+
+        result = Menu.menuValidIntegerCheckerWithDefault(-100, null, prompt, errorMessage, 100);
+        printResult(result);
+
+        result = Menu.menuValidIntegerCheckerWithDefault(-100, 0, prompt, errorMessage, 100);
+        printResult(result);
     }
 }
+
